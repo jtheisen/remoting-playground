ccf6591851fa4b48e2a9d992404960595ba0a419
Jens Theisen <jens.theisen@visbricks.com>
2018-12-05 13:13:59 +0100

    Make the serializer work accross different frameworks (works with our three sample calls)

--- Aq.ExpressionJsonSerializer/Deserializer/Deserializer.Reflection.cs
+++ Aq.ExpressionJsonSerializer/Deserializer/Deserializer.Reflection.cs
@@ -16,35 +16,52 @@ namespace Aq.ExpressionJsonSerializer
 
         private Type Type(JToken token)
         {
-            if (token == null || token.Type != JTokenType.Object) {
+            if (token == null || token.Type != JTokenType.Object)
+            {
                 return null;
             }
 
-            var obj = (JObject) token;
+            var obj = (JObject)token;
             var assemblyName = this.Prop(obj, "assemblyName", t => t.Value<string>());
             var typeName = this.Prop(obj, "typeName", t => t.Value<string>());
             var generic = this.Prop(obj, "genericArguments", this.Enumerable(this.Type));
 
             Dictionary<string, Dictionary<string, Type>> assemblies;
-            if (!TypeCache.TryGetValue(this._assembly, out assemblies)) {
+            if (!TypeCache.TryGetValue(this._assembly, out assemblies))
+            {
                 assemblies = new Dictionary<string, Dictionary<string, Type>>();
                 TypeCache[this._assembly] = assemblies;
             }
 
             Dictionary<string, Type> types;
-            if (!assemblies.TryGetValue(assemblyName, out types)) {
+            if (!assemblies.TryGetValue(assemblyName, out types))
+            {
                 types = new Dictionary<string, Type>();
                 assemblies[assemblyName] = types;
             }
 
             Type type;
-            if (!types.TryGetValue(typeName, out type)) {
+            if (!types.TryGetValue(typeName, out type))
+            {
                 type = this._assembly.GetType(typeName);
-                if (type == null) {
-                    var assembly = Assembly.Load(new AssemblyName(assemblyName));
+                if (type == null)
+                {
+                    var parsedAssemblyName = new AssemblyName(assemblyName);
+
+                    if (parsedAssemblyName.Name == "System.Private.CoreLib")
+                    {
+                        parsedAssemblyName = new AssemblyName("netstandard");
+                    }
+                    else if (parsedAssemblyName.Name == "System.Linq.Expressions" && typeName == "System.Linq.IQueryable`1")
+                    {
+                        parsedAssemblyName = new AssemblyName("netstandard");
+                    }
+
+                    var assembly = Assembly.Load(parsedAssemblyName);
                     type = assembly.GetType(typeName);
                 }
-                if (type == null) {
+                if (type == null)
+                {
                     throw new Exception(
                         "Type could not be found: "
                         + assemblyName + "." + typeName
@@ -53,7 +70,8 @@ namespace Aq.ExpressionJsonSerializer
                 types[typeName] = type;
             }
 
-            if (generic != null && type.IsGenericTypeDefinition) {
+            if (generic != null && type.IsGenericTypeDefinition)
+            {
                 type = type.MakeGenericType(generic.ToArray());
             }
 
@@ -62,11 +80,12 @@ namespace Aq.ExpressionJsonSerializer
 
         private ConstructorInfo Constructor(JToken token)
         {
-            if (token == null || token.Type != JTokenType.Object) {
+            if (token == null || token.Type != JTokenType.Object)
+            {
                 return null;
             }
 
-            var obj = (JObject) token;
+            var obj = (JObject)token;
             var type = this.Prop(obj, "type", this.Type);
             var name = this.Prop(obj, "name").Value<string>();
             var signature = this.Prop(obj, "signature").Value<string>();
@@ -75,9 +94,10 @@ namespace Aq.ExpressionJsonSerializer
             Dictionary<string, ConstructorInfo> cache2;
             Dictionary<string, Dictionary<string, ConstructorInfo>> cache1;
 
-            if (!ConstructorCache.TryGetValue(type, out cache1)) {
+            if (!ConstructorCache.TryGetValue(type, out cache1))
+            {
                 constructor = this.ConstructorInternal(type, name, signature);
-                
+
                 cache2 = new Dictionary<
                     string, ConstructorInfo>(1) {
                         {signature, constructor}
@@ -88,12 +108,13 @@ namespace Aq.ExpressionJsonSerializer
                         string, ConstructorInfo>>(1) {
                             {name, cache2}
                         };
-                
+
                 ConstructorCache[type] = cache1;
             }
-            else if (!cache1.TryGetValue(name, out cache2)) {
+            else if (!cache1.TryGetValue(name, out cache2))
+            {
                 constructor = this.ConstructorInternal(type, name, signature);
-                
+
                 cache2 = new Dictionary<
                     string, ConstructorInfo>(1) {
                         {signature, constructor}
@@ -101,7 +122,8 @@ namespace Aq.ExpressionJsonSerializer
 
                 cache1[name] = cache2;
             }
-            else if (!cache2.TryGetValue(signature, out constructor)) {
+            else if (!cache2.TryGetValue(signature, out constructor))
+            {
                 constructor = this.ConstructorInternal(type, name, signature);
                 cache2[signature] = constructor;
             }
@@ -115,13 +137,15 @@ namespace Aq.ExpressionJsonSerializer
             var constructor = type
                 .GetConstructors(BindingFlags.Public | BindingFlags.Instance)
                 .FirstOrDefault(c => c.Name == name && c.ToString() == signature);
-            
-            if (constructor == null) {
+
+            if (constructor == null)
+            {
                 constructor = type
                     .GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance)
                     .FirstOrDefault(c => c.Name == name && c.ToString() == signature);
-                
-                if (constructor == null) {
+
+                if (constructor == null)
+                {
                     throw new Exception(
                         "Constructor for type \""
                         + type.FullName +
@@ -137,11 +161,12 @@ namespace Aq.ExpressionJsonSerializer
 
         private MethodInfo Method(JToken token)
         {
-            if (token == null || token.Type != JTokenType.Object) {
+            if (token == null || token.Type != JTokenType.Object)
+            {
                 return null;
             }
 
-            var obj = (JObject) token;
+            var obj = (JObject)token;
             var type = this.Prop(obj, "type", this.Type);
             var name = this.Prop(obj, "name").Value<string>();
             var signature = this.Prop(obj, "signature").Value<string>();
@@ -153,7 +178,8 @@ namespace Aq.ExpressionJsonSerializer
             );
             var method = methods.First(m => m.Name == name && m.ToString() == signature);
 
-            if (generic != null && method.IsGenericMethodDefinition) {
+            if (generic != null && method.IsGenericMethodDefinition)
+            {
                 method = method.MakeGenericMethod(generic.ToArray());
             }
 
@@ -162,11 +188,12 @@ namespace Aq.ExpressionJsonSerializer
 
         private PropertyInfo Property(JToken token)
         {
-            if (token == null || token.Type != JTokenType.Object) {
+            if (token == null || token.Type != JTokenType.Object)
+            {
                 return null;
             }
 
-            var obj = (JObject) token;
+            var obj = (JObject)token;
             var type = this.Prop(obj, "type", this.Type);
             var name = this.Prop(obj, "name").Value<string>();
             var signature = this.Prop(obj, "signature").Value<string>();
@@ -180,15 +207,16 @@ namespace Aq.ExpressionJsonSerializer
 
         private MemberInfo Member(JToken token)
         {
-            if (token == null || token.Type != JTokenType.Object) {
+            if (token == null || token.Type != JTokenType.Object)
+            {
                 return null;
             }
 
-            var obj = (JObject) token;
+            var obj = (JObject)token;
             var type = this.Prop(obj, "type", this.Type);
             var name = this.Prop(obj, "name").Value<string>();
             var signature = this.Prop(obj, "signature").Value<string>();
-            var memberType = (MemberTypes) this.Prop(obj, "memberType").Value<int>();
+            var memberType = (MemberTypes)this.Prop(obj, "memberType").Value<int>();
 
             var members = type.GetMembers(
                 BindingFlags.Public | BindingFlags.NonPublic |
